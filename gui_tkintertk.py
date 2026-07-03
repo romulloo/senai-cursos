bace24a3e6f73f693e62d2024b8ceadb1f64ac33
Romullo Soares <soaresromullo227@gmail.com>
2026-07-02 21:15:56 -0300

    Janela Principal

--- gui_tkintertk.py
+++ gui_tkintertk.py
@@ -1,1 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
 
+
+janela = tk.Tk()
+janela.title("Meu App")
+janela.geometry("400x200")#Largura x altura em pixels
+
+label = ttk.Label(janela, text="Ola!")
+label.pack(pady=20, padx=20)
+texto = ttk.Entry(janela, width=20)
+texto.pack(pady=30)
+#Loop eventos
+janela.mainloop()
